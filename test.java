07748532968c80d8616d675b070bfcf5ddf60efc
Suraj Paliwal <86527658+surajpaliwal@users.noreply.github.com>
2022-11-21 10:41:41 +0530

    Update test.java

--- test.java
+++ test.java
@@ -20,6 +20,7 @@ public class test {
 
         System.out.println("--After swap--");
         System.out.println("First number = " + first);
+        System.out.println("First number = " + first);
         System.out.println("Second number = " + second);
         System.out.println("Second number = " + second);
 
